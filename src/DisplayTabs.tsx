02a3925aff371c93f0bf28bca2ff4396fcbb70f2
Brett <bretthoganpr@gmail.com>
2022-08-03 02:51:56 -0400

    Replaced amount per row with inline wrap

--- src/DisplayTabs.tsx
+++ src/DisplayTabs.tsx
@@ -5,51 +5,29 @@ import { TabShow } from "./TabShow"
 export function DisplayTabs(props: {
     tabs: string[][]
 }) {
-    const [rows, setrows] = useState<string[][][]>([])
     const [pad, setpad] = useState<number>(0)
 
-    const TabsPerRow = 8
-
     useEffect(() => {
-        const newRows: string[][][] = []
-
         const count = props.tabs.length
         setpad(count.toString().length)
-
-        props.tabs.map((tab, i) => {
-            const n = i % TabsPerRow
-            const t = (Math.floor(i / TabsPerRow))
-
-            if (n === 0)
-                newRows[t] = []
-
-            newRows[t][n] = tab
-        })
-
-        setrows(newRows)
     }, [props.tabs])
 
     return <div style={{
-        display: "flex",
-        flexDirection: "column"
+        display: "inline-flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        padding: "0px",
+        margin: "0px",
+        border: "1px soild white",
+        color: "white",
+        maxWidth: "50vw"
     }}>
-        {rows.map((row, rowN) => {
-            return <div style={{
-                display: "flex",
-                flexDirection: "row"
-            }}>
-                {row.map((tab, i) => {
-                    const tabN = rowN * TabsPerRow + i
-                    return <TabShow
-                        title={
-                            `${tabN + 1}`.padStart(pad, "0")
-                            +
-                            `/${props.tabs.length}`
-                        }
-                        tabs={tab}
-                    />
-                })}
-            </div>
+        {props.tabs.map((tabSection, i) => {
+            return <TabShow
+                title={`Tab ${i + 1}/${props.tabs.length}`}
+                key={i}
+                tabs={tabSection}
+            />
         })}
     </div>
 }
