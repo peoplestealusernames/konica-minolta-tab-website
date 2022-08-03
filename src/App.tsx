c2c5b845b798b140be8ee2d8b8fb4235d634aa5d
Brett <bretthoganpr@gmail.com>
2022-08-02 23:09:50 -0400

    Tab spliter

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,24 @@ import './App.css';
 
 function App() {
   const [input, setinput] = useState<string>("")
+  const [tabs, settabs] = useState<string[][]>([])
+
+  useEffect(() => {
+    const lines = input.split("\n")
+    const newTab: string[][] = []
+
+    lines.map((line, i) => {
+      const n = i % 20
+      const t = (Math.floor(i / 20))
+
+      if (n === 0)
+        newTab[t] = []
+
+      newTab[t][n] = line
+    })
+
+    settabs(newTab)
+  }, [input])
 
   return (
     <div className='App'>
