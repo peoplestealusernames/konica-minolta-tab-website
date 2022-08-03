a2f0e35583e180b73a6e3f065c4e2371ad9728e9
Brett <bretthoganpr@gmail.com>
2022-08-02 22:17:44 -0400

    Init: textarea on left side of screen

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <textarea
+        style={{
+          display: "flex",
+          width: "50vw",
+          height: "100vh",
+          backgroundColor: "black",
+          color: "white"
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
