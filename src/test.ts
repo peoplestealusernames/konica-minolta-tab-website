f2f4e7877dc7cc9daa8df54250f3224e34747fee
Brett <bretthoganpr@gmail.com>
2022-08-03 04:44:24 -0400

    Ported tab maker

--- src/test.ts
+++ src/test.ts
@@ -1,32 +1,24 @@
-import { appendFileSync, existsSync, mkdirSync, readFileSync, rmSync, writeFileSync, } from "fs";
+import TabString from "./TabExport"
 
-const data = readFileSync('names.txt', { encoding: 'utf8' })
+export type fileReturn = { name: string, write: string }
 
-let output: [string[]] = [[]]
+export function MakeTabs(tabNames: string[][]): fileReturn[] {
+    const files: fileReturn[] = []
 
-const lines = data.split('\n');
-for (let i = 0; i < lines.length; i++) {
-    if (!output[Math.floor((i) / 20)])
-        output[Math.floor((i) / 20)] = []
+    for (let k = 0; k < tabNames.length; k++) {
+        let stri = TabString
+        stri = stri.replaceAll("{Name}", `Tabs_${k + 1}/${tabNames.length}`)
+        stri = stri.replace("{Count}", tabNames[k].length.toString())
 
-    output[Math.floor((i) / 20)][i % 20] = lines[i]
-}
+        for (let i = 0; i < tabNames[k].length; i++) {
+            stri = stri.replace(`{${i + 1}}`, tabNames[k][i])
+        }
 
-if (existsSync('./Out'))
-    rmSync('./Out', { 'recursive': true })
-
-mkdirSync('./Out')
-
-for (let k = 0; k < output.length; k++) {
-    let stri = readFileSync("TabExport.KSF", { encoding: 'utf-8' })
-    stri = stri.replace("{Name}", `Tabs_${k + 1}/${output.length}`)
-    stri = stri.replace("{Name}", `Tabs_${k + 1}/${output.length}`)
-    stri = stri.replace("{Name}", `Tabs_${k + 1}/${output.length}`)
-    stri = stri.replace("{Count}", output[k].length.toString())
-
-    for (let i = 0; i < output[k].length; i++) {
-        stri = stri.replace(`{${i + 1}}`, output[k][i])
+        files.push({
+            name: `./Out/Tabs_${k + 1}_of_${tabNames.length}.KSF`,
+            write: stri
+        })
     }
 
-    writeFileSync(`./Out/Tabs_${k + 1}_of_${output.length}.KSF`, stri)
+    return files
 }
