7e3ef9ce2132d6864cdcc6fc4f078bdd15293ed8
Brett <bretthoganpr@gmail.com>
2022-08-04 08:07:26 -0400

    Swapped textarea for editor

--- src/PastePopup.tsx
+++ src/PastePopup.tsx
@@ -1,19 +1,21 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { Popup } from "./components/popup/Popup";
 import { StyledTab } from "./components/popup/StyledTab";
+import { Editor } from "./Editor";
 
 
 export function PastePopup(props: {
     active: boolean
     setActive: Dispatch<SetStateAction<boolean>>,
 }) {
+    const [paths, setpaths] = useState<string>("")
 
     return <Popup
         active={props.active}
         setActive={props.setActive}
     >
         <StyledTab title="Paste paths">
-            <textarea />
+            <Editor lineLength={1000} value={paths} onChange={setpaths} />
         </StyledTab>
     </Popup>
 }
