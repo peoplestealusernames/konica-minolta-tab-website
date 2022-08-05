e08b67998ad4cc368fdf7bb4f1816ebd7c227eb6
Brett <bretthoganpr@gmail.com>
2022-08-05 03:50:07 -0400

    Added option styles to blur background

--- src/components/popup/BlurBackground.tsx
+++ src/components/popup/BlurBackground.tsx
@@ -1,11 +1,14 @@
+import React from "react"
+
 export function BlurBackground(props: {
     children?: React.ReactNode
+    style?: React.CSSProperties
 }) {
     return (
         <div style={{
-            position: "absolute",
             display: "flex",
             background: "rgba(0, 0, 30, 0.5)",
+            ...props.style
         }}>
             {props.children}
         </div>
