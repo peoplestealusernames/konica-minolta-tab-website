68ffb8a3d1bccc0755af3a50bf8d81290f737e6c
Brett <bretthoganpr@gmail.com>
2022-08-05 18:41:26 -0400

    Restyle on CloseButton

--- src/components/popup/CloseButton.tsx
+++ src/components/popup/CloseButton.tsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineClose } from "react-icons/md";
+import { ContextButton } from "../ContextButton";
 
 export function CloseButton(props: {
     onClose: () => void
     stye?: React.CSSProperties
 }) {
+    const style: React.CSSProperties = {
+        position: "absolute",
+        right: "3px",
+        top: "3px",
+        color: "grey",
+        height: "20px",
+        width: "20px",
+        transition: "color 200ms linear",
+        ...props.stye
+    }
+
     return (
-        <MdOutlineClose style={{
-            position: "absolute",
-            right: "3px",
-            top: "3px",
-            color: "grey",
-            height: "20px",
-            width: "20px",
-            ...props.stye
-        }}
-            onClick={props.onClose}
-        />
+        <ContextButton style={style} hoverStyle={{ color: "red" }}>
+            <MdOutlineClose
+                style={{ display: "flex", width: "100%", height: "100%" }}
+                onClick={props.onClose}
+            />
+        </ContextButton>
     )
 }
