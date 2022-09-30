7f32e68364256f0d76f2d92c214246020a3e4014
Brett <bretthoganpr@gmail.com>
2022-09-29 21:18:35 -0400

    PrintButton: Overhaul

--- src/printing/PrintButton.tsx
+++ src/printing/PrintButton.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import { BsFillPrinterFill } from "react-icons/bs"
 import { ContextButton } from "../components/ContextButton";
 
@@ -17,23 +19,31 @@ export function PrintButton(props: {
 
         const padding = Math.floor(Math.log10(lines.length)) + 1
 
-        lines.forEach((e, i) => {
-            lines[i] = `${(i + 1).toString().padStart(padding, "0")
-                }:   ${lines[i]}`
-        });
-
-        let text = lines.join("<br/>")
-
         printWindow.document.open("text/plain")
-        printWindow.document.write("<html><style type='text/css' media='print'>")
-        printWindow.document.write("@page { size: auto; margin: 30px 20px 30px 20px; }")
-        printWindow.document.write("footer {display:none; }")
-        printWindow.document.write("header {display:none; }")
-        printWindow.document.write("html { background-color: #FFFFFF; margin: 0; }")
-        printWindow.document.write("body { display: block; margin: margin: 0; font-size: 20px;}")
-        printWindow.document.write("</style><title>Index maker</title><body>")
-        printWindow.document.write(text);
-        printWindow.document.write("</body></html>")
+        printWindow.document.write("<!DOCTYPE html>")
+        ReactDOM.render(
+            <html>
+                <head>
+                    <style>
+                        {`
+                        @page {
+                            margin: 0px
+                        }
+                        `}
+                    </style>
+                </head>
+                <div style={{ margin: "5px" }}>
+                    {lines.map((e, i) => <div style={{
+                        width: "100%",
+                        borderBottom: "1px solid black",
+                    }}>
+                        {`${(i + 1).toString().padStart(padding, "0")
+                            }:   ${e}`}
+                    </div>
+                    )}
+                </div>
+            </html>
+            , printWindow.document)
 
         printWindow.focus();
         printWindow.print();
