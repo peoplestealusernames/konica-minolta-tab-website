53ab67d16e3d5bc0695bdfd88a35652dad923f4c
Brett <bretthoganpr@gmail.com>
2022-09-29 21:49:03 -0400

    TabPage: Changed width back to 9in from 8.5in

--- src/printing/TabPage.tsx
+++ src/printing/TabPage.tsx
@@ -46,7 +46,7 @@ function Page(props: {
         position: "relative",
         margin: "0in",
         padding: "0px",
-        width: "8.5in",//TODO: change back
+        width: "9in",
         height: "11in",
         backgroundColor: "white",
         ...props.style
